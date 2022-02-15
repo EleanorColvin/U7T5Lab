ed2440d74576a4c01d69d1c9c1a43317abdf3bbf
BT_1E10_10 <eleanorc25@nycstudents.net>
2022-02-15 10:59:42 -0500

    method changes

--- src/Search.java
+++ src/Search.java
@@ -6,44 +6,79 @@ public class Search {
     // a target int is found in an array of ints (both the target and int array
     // should be accepted as parameters); return -1 if target is not found
     // BE SURE TO USE EARLY RETURN (more efficient)
-
-
-
-
+    public static int linearSearch(int[] intArray, int target)
+    {
+        for (int i = 0; i < intArray.length; i++)
+        {
+            if (intArray[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     // B. write a second static method that has the same parameters as linearSearch but
     // instead of returning an index, it returns true if target is found or false
     // if target is not found
     // AGAIN, BE SURE TO USE EARLY RETURN
-
-
-
-
+    public static boolean searchTest(int[] intArray, int target)
+    {
+        for (int num : intArray)
+        {
+            if (num == target)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
     // C. provide an OVERLOADED version of the linearSearch method that accepts an ArrayList
     // of Integers (rather than an array of ints) and a target int; return the first index
     // at which the target is found, or return -1 if target is not found
     // BE SURE TO USE EARLY RETURN
-
-
-
-
-
+    public static int linearSearch(ArrayList<Integer> intList, int target)
+    {
+        for (int i = 0; i < intList.size(); i++)
+        {
+            if (intList.get(i) == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
     // D. provide another OVERLOADED version of the linearSearch method that accepts an array
     // of Strings and a target String; return the first index at which the target is found,
     // or return -1 if target is not found
     // BE SURE TO USE EARLY RETURN
-
-
-
-
+    public static int linearSearch(String[] stringArray, String target)
+    {
+        for (int i = 0; i < stringArray.length; i++)
+        {
+            if (stringArray[i].equals(target))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     // E. write a "linearSearchLast" method that accepts an array of ints and a
     // target int, and return the LAST index at which the target is found,
     // or return -1 if target is not found
-
-
-
+    public static int linearSearchLast(int[] intArr, int target)
+    {
+        for (int i = intArr.length - 1; i >= 0; i--)
+        {
+            if (intArr[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 
     // F. write two static "linearSearchCount" methods (both overloaded), one that
@@ -51,6 +86,18 @@ public class Search {
     // of Integers and a target int.  Each method should return the NUMBER OF TIMES
     // the target appears in the array/ArrayList.
 
+    public static int linearSearchCount(int[] intArr, int target)
+    {
+        int times = 0;
+        for (int i = 0; i < intArr.length; i++)
+        {
+            if (intArr[i] == target)
+            {
+                times++;
+            }
+        }
+        return times;
+    }
 
 
 
